506835f9d8dd771a882108600d2c479ad62e6646
voomin kim <qnals9313@naver.com>
2020-02-09 09:04:15 +0900

    변수명 변경

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,19 +1,29 @@
 const R=(min,max)=> parseInt(Math.random()*(max-min))+min;//min <= return < max
-const set={
-    block:35,//캐릭터 사이즈(전체캐릭터)
-    SPD:R(0,4),//default
-    Maxspd:R(3,9),//최고속력
-    spd:this.SPD,//현재속력
-    a:R(1,4)/10,//가속도
-    map_width:1000,
-    map_height:700,
+const CONFIG = {
+    block_size: 35,
+    MAP:{
+        width: 1000,
+        height: 700
+    }
+}
+const _MINE={
+    user_name: null,
+    px: [0,0],
+    WENS: [false,false,false,false], //west, east, north, south
+    SPD: R(0,4),//default
+    Maxspd: R(3,9),//최고속력
+    spd: this.SPD,//현재속력
+    a: R(1,4)/10, //가속도
     //G:true//중력
-  }
+}
 const idSet=(id)=> id.replace(/\./g,"x").replace(/\:/g,"");
-var myid=null;
-var socket = io(); 
+const UserPosition= (id,px) => $("#u"+id).css({ marginLeft : px[0], marginTop : px[1] } );
+const socket = io(); 
+let thread=null;
+
 document.addEventListener("touchstart", touchHandler);
 document.addEventListener("touchmove", touchHandler);
+
 function touchHandler(e) {
     if(e.touches) {
         playerX = e.touches[0].pageX - canvas.offsetLeft - playerWidth / 2;
@@ -22,18 +32,9 @@ function touchHandler(e) {
     }
 }
 $(function(){
-    
-  $('.map').css({ width: set.map_width, height: set.map_height }); 
-  $('h1').click(function(){
-    //$(location).attr('href', '/');
-    });
-  $('.array').click(function(){
-    $(".city").slideToggle(1000);
-    });
-  $('#chat .chat').click(function(e){ //2
-    sendMSG();
-  });
-  $("#name").change("input",function(){
+  $('.map').css({ width: CONFIG.MAP.width, height: CONFIG.MAP.height }); 
+  $('#chat .chat').click(sendMSG);
+  $("#name").change("input",() => {
     socket.emit('change name', $('#name').val());
   });
 });
@@ -43,31 +44,28 @@ socket.on('receive name', function(id,name){ //4
 socket.on('receive message', function(id,msg){ //3
     id=idSet(id);
     receiveMSG(id,msg);
-    //$('.log textarea').append(msg+'\n');
-    //$('.log textarea').scrollTop($('.log textarea')[0].scrollHeight);
-
 });
 socket.on('Existing users', function(arr){
     for(i in arr)
         createUser(arr[i]);
-    arraylength();
+    // arraylength();
 });
 socket.on('My user', function(obj){
-    myid=idSet(obj.id);
+    _MINE.user_name = idSet(obj.id);
     px=obj.px;
     $('#name').val(obj.name);
-    if(thread==null)
+    if(thread===null)
         thread=setInterval(move,10);
 });
 socket.on('New user', function(obj){
     createUser(obj);
-    arraylength();
+    // arraylength();
 });
 socket.on('Out user', function(id){
     id=idSet(id);
     $('#p'+id).fadeOut(1000).remove();
     $('#u'+id).remove();
-    arraylength();
+    // arraylength();
 });
 socket.on('receive move user', function(id,px){
     id=idSet(id);
@@ -79,10 +77,8 @@ function receiveMSG(id,msg){
     }else{
         $('#u'+id+" .talk").show();
         $('#u'+id+" .talk").html(msg);
-        $('#u'+id+" .talk").css("left", -($('#u'+id+" .talk").width()/2)+(set.block/2));
-        //$('#u'+id+" .talk").animate({top: -$('#u'+id+" .talk").height()+15});
+        $('#u'+id+" .talk").css("left", -($('#u'+id+" .talk").width()/2)+(CONFIG.block_size/2));
         $('#u'+id+" .talk").css("top", -$('#u'+id+" .talk").height()+15);
-
     }
 }
 function sendMSG(){
@@ -97,9 +93,6 @@ function nameSet(id,name){
     $('#p'+id).html(name);
     $('#u'+id).attr("title","ip = "+id+"\nname = "+name);
 }
-function arraylength(){
-    $(".array").html("호모목록 ("+$( ".city .people" ).length+")");
-}
 function createUser(obj){
     obj.id=idSet(obj.id);
     $( ".city" ).append( "<div>"+obj.name+"</div>" );
@@ -112,102 +105,97 @@ function createUser(obj){
         +"</div>" );
 
     $('#u'+obj.id+" .talk").hide();
-    $('.map .u img').width(set.block);
-    $('.map .u img').height(set.block);
+    $('.map .u img').width(CONFIG.block_size);
+    $('.map .u img').height(CONFIG.block_size);
 
     nameSet(obj.id,obj.name);
     receiveMSG(obj.id,obj.msg);
     UserPosition(obj.id,obj.px);
-    
-    
 }
-var thread=null;
-var UserPosition= (id,px) => $("#u"+id).css({ marginLeft : px[0], marginTop : px[1] } );
-var point=[0,0,0,0];
-var px=[0,0];
-                document.onkeydown=function(event){
-                    switch(event.keyCode){
-                        case 37://left
-                            point[0]=1;
-                            if(point[1]){
-                                point[1]=0;
-                                set.spd=set.SPD;
-                            }
-                            break;
-                        case 39://right
-                            if(point[0]){
-                                point[0]=0;
-                                set.spd=set.SPD;
-                            }
-                            point[1]=1;
-                            break;
-                        case 40://top
-                            point[2]=1;
-                            if(point[3]){
-                                point[3]=0;
-                                set.spd=set.SPD;
-                            }
-                            break;
-                        case 38://bottom
-                                if(point[2]){
-                                    point[2]=0;
-                                    set.spd=set.SPD;
-                                }
-                                point[3]=1;
-                            break;
-                    }
-                    if(set.spd<=set.Maxspd)
-                        set.spd+=set.a;
-                }
-                document.onkeyup=function(event){//키 눌렀다 땔때
-                    switch(event.keyCode){
-                        case 37:
-                            point[0]=0;
-                            break;
-                        case 39:
-                            point[1]=0;
-                            break;
-                        case 40:
-                            point[2]=0;
-                            break;
-                        case 38:
-                                point[3]=0;
 
-                            break;
-                        case 13:
-                            sendMSG();
-                            break;
-                    }
-                    set.spd=set.SPD;
+document.onkeydown=function(event){
+    switch(event.keyCode){
+        case 37://left
+            _MINE.WENS[0]=true;
+            if(_MINE.WENS[1]){
+                _MINE.WENS[1]=false;
+                _MINE.spd=_MINE.SPD;
+            }
+            break;
+        case 39://right
+            if(_MINE.WENS[0]){
+                _MINE.WENS[0]=false;
+                _MINE.set.spd=_MINE.set.SPD;
+            }
+            _MINE.WENS[1]=true;
+            break;
+        case 40://top
+        _MINE.WENS[2]=true;
+            if(_MINE.WENS[3]){
+                _MINE.WENS[3]=false;
+                _MINE.spd=_MINE.SPD;
+            }
+            break;
+        case 38://bottom
+                if(_MINE.WENS[2]){
+                    _MINE.WENS[2]=false;
+                    _MINE.spd=_MINE.SPD;
                 }
-                function move(){
-                    var spd=(typeof(set.spd)=="number")?set.spd:0;
-                    if(point[0]){
-                        px[0]-=spd;
-                    }
-                    if(point[1]){
-                        px[0]+=spd;
-                    }
-                    if(point[2]){
-                        px[1]+=spd;
-                    }
-                    if(point[3]){
-                        px[1]-=spd;
-                    }
-        
-                    if(px[0]<0){
-                        px[0]=0;
-                    }
-                    if(px[1]<0){
-                        px[1]=0;
-                    }
-                    if(this.px[0]>set.map_width-set.block){
-                        this.px[0]=set.map_width-set.block;
-                    }
-                    if(this.px[1]>set.map_height-set.block){
-                        this.px[1]=set.map_height-set.block;
-                    }
-                    UserPosition(myid,px);
-                    if(point[0]||point[1]||point[2]||point[3])
-                        socket.emit('move user',px);
-                }
+                _MINE.WENS[3]=true;
+            break;
+    }
+    if(_MINE.spd<=_MINE.Maxspd)
+        _MINE.spd+=_MINE.a;
+}
+document.onkeyup=function(event){//키 눌렀다 땔때
+    switch(event.keyCode){
+        case 37:
+            _MINE.WENS[0]=false;
+            break;
+        case 39:
+            _MINE.WENS[1]=true;
+            break;
+        case 40:
+            _MINE.WENS[2]=false;
+            break;
+        case 38:
+            _MINE.WENS[3]=false;
+
+            break;
+        case 13:
+            sendMSG();
+            break;
+    }
+    _MINE.spd=_MINE.SPD;
+}
+function move(){
+    var spd=(typeof(_MINE.spd)=="number")?_MINE.spd:0;
+    if(_MINE.WENS[0]){
+        _MINE.px[0]-=spd;
+    }
+    if(_MINE.WENS[1]){
+        _MINE.px[0]+=spd;
+    }
+    if(_MINE.WENS[2]){
+        _MINE.px[1]+=spd;
+    }
+    if(_MINE.WENS[3]){
+        _MINE.px[1]-=spd;
+    }
+
+    if(_MINE.px[0]<0){
+        _MINE.px[0]=0;
+    }
+    if(_MINE.px[1]<0){
+        _MINE.px[1]=0;
+    }
+    if(_MINE.px[0]>CONFIG.MAP.width-CONFIG.block_size){
+        _MINE.px[0]=CONFIG.MAP.width-CONFIG.block_size;
+    }
+    if(_MINE.px[1]>CONFIG.MAP.height-CONFIG.block_size){
+        _MINE.px[1]=CONFIG.MAP.height-CONFIG.block_size;
+    }
+    UserPosition(_MINE.user_name,_MINE.px);
+    if(_MINE.WENS[0]||_MINE.WENS[1]||_MINE.WENS[2]||_MINE.WENS[3])
+        socket.emit('move user', _MINE.px);
+}
